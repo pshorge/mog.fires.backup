5750c072a0c98efe54565930d039e1d129255cb5
Paweł Hanejko <phanejko@newamsterd.am>
2025-11-12 19:00:05 +0100

    preserve global ui converter from stripping, register once garding, add bool? to control from uss

--- mog.fires/Assets/Sources/Presentation/UI/Converters/UIConverters.cs
+++ mog.fires/Assets/Sources/Presentation/UI/Converters/UIConverters.cs
@@ -1,19 +1,40 @@
-using UnityEditor;
+using UnityEngine.Scripting;
 using UnityEngine.UIElements;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Sources.Presentation.UI.Converters
 {
     public static class UIConverters
     {
+        private static bool _registered;
+
+        [Preserve]  
 #if UNITY_EDITOR
         [InitializeOnLoadMethod]
 #else
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
 #endif
-        public static void RegisterConverters()
+        private static void RegisterConverters()
         {
+            if (_registered) return;
+            _registered = true;
+
+            // bool -> StyleEnum<DisplayStyle> (true => Flex, false => None)
             ConverterGroups.RegisterGlobalConverter(
-                (ref bool value) => (StyleEnum<DisplayStyle>)(value ? DisplayStyle.Flex: DisplayStyle.None));
+                (ref bool value) => value ? DisplayStyle.Flex : DisplayStyle.None
+            );
+
+            // bool? -> StyleEnum<DisplayStyle>
+            // null => StyleKeyword.Null (return control to USS), true => Flex, false => None
+            ConverterGroups.RegisterGlobalConverter(
+                (ref bool? value) =>
+                    value.HasValue
+                        ? (StyleEnum<DisplayStyle>)(value.Value ? DisplayStyle.Flex : DisplayStyle.None)
+                        : (StyleEnum<DisplayStyle>)StyleKeyword.Null
+            );
         }
     }
+
 }
